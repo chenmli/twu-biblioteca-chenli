76b2f685082b6208652748dc28ccce31cf81f805
chenmli <35786460+chenmli@users.noreply.github.com>
2020-02-07 11:02:22 -0600

    Story 1 added Welcome Message

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -1,8 +1,49 @@
 package com.twu.biblioteca;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class BibliotecaApp {
 
-    public static void main(String[] args) {
-        System.out.println("Hello, world!");
+    private List<Book> bookList;
+
+    public BibliotecaApp(List<Book> bookList)
+    {
+        this.bookList = bookList;
+        this.bookList.add(new Book("Childhood of Logan"));
+        this.bookList.add(new Book("Adulthood of Aaron"));
+        this.bookList.add(new Book("Neighborhood of Paris"));
+
     }
+
+    public List<Book> getBookList() {
+        return bookList;
+    }
+
+    public void setBookList(List<Book> bookList) {
+        this.bookList = bookList;
+    }
+
+    // Displays Welcome Message
+    public void displayWelcomeMessage()
+    {
+        System.out.println("Welcome to Biblioteca. Your one-stop-shop for great book titles in Bangalore!");
+    }
+
+    // Displays Booklist
+    public void displayBookList()
+    {
+
+        for (Book book:bookList) {
+            System.out.println(book.getBookName());
+        }
+    }
+    public static void main(String[] args) {
+
+        BibliotecaApp bibliotecaApp = new BibliotecaApp(new ArrayList<Book>());
+        bibliotecaApp.displayWelcomeMessage();
+        bibliotecaApp.displayBookList();
+
+    }
+
 }
